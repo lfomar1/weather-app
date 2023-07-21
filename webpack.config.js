5f71bfb920ee7565a6e52b92bf0d915747d6ba1a
Lucas <107668539+lfomar1@users.noreply.github.com>
2023-07-21 00:09:30 -0400

    Basic structure html, css, done

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
   },
 };
